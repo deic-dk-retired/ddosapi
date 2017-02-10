17a0b29d7eb29e6b309472233a8639f50e55434b
rnd <ashokaditya.design@gmail.com>
2017-02-10 10:56:46 +0100

    almost curling without curl modules

--- fnm.js
+++ fnm.js
@@ -5,59 +5,56 @@
 
 var path = require('path');
 var fs = require('fs');
-// var rp = require('request-promise');
 var got = require('got');
-var queryapi = 'http://172.22.89.2:8086/query?q=';
-// const Influx = require('influxdb-nodejs');
-// const client = new Influx('http://172.22.89.2:8086/graphite');
+var curlToNode = require('curl-to-node');
+
+var queryapi = "http://172.22.89.2:8086/query"
+  // , influxql = ""
+  , urlquery = "q="
+  , curlstr;
+
+function curlStr(file){
+  // console.log("file: " + file);
+  fs.readFile(file, 'utf8', function (err, fdata) {
+    if (err) {throw err};
+    // influxql = fdata;
+    curlstr = 'curl -G '+ '\'' + queryapi + '\'' + ' --data-urlencode ' + '\"' + urlquery + fdata + '\"';
+  });
+  // console.log("influxql: " + influxql);
+  console.log("curlstr: " + curlstr);
+  return curlstr;
+}
 
 //read json data from fastnetmon instance
 //using curl or something similar
 //catch the json output pass it on here.
 function getSeries(req, res, next) {
   var file = req.params.series +'.txt';
-  var influxql;
-  console.log("file: " + file);
-  var influxql = fs.readFile(file, 'utf8', function (err, fdata) {
-    if (err) {throw err};
-    influxql = fdata;
-    // console.log("fdata: " + fdata);
-    console.log("influxql: " + influxql);
-    queryapi += influxql;
-  });
-
-  // var options = {
-  //     uri: queryapi,
-  //     headers: {
-  //         'User-Agent': 'Request-Promise'
-  //     },
-  //     json: true // Automatically parses the JSON string in the response
-  // };
-
-  // rp(options)
+  var str = curlStr(file);
+  got.apply(got, curlToNode('got', str ))
+    .then(response => {
+      response.status(200);
+      console.log("response: "+response.body);
+    })
+    .catch(error => {
+      console.log("err: "+error.response.body);
+    });
+  // got(queryapi)
   //   .then(function (data) {
+  //     // console.log("queryapi th: " + urlquery);
   //     res.status(200)
-  //     console.log(data);
+  //        .json({
+  //         status: 'success',
+  //         data: data,
+  //         message: 'Retrieved requested series'
+  //       });
   //   })
   //   .catch(function (err) {
+  //     // console.log("queryapi er: " + urlquery);
   //     return next(err.message);
   //   });
-
-  got(queryapi)
-    .then(function (data) {
-      console.log("queryapi: " + queryapi);
-      res.status(200)
-         .json({
-          status: 'success',
-          data: data,
-          message: 'Retrieved requested series'
-        });
-    })
-    .catch(function (err) {
-      console.log("queryapi: " + queryapi);
-      return next(err.message);
-    });
 }
+
 module.exports = {
   getSeries: getSeries
 // ,
