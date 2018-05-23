07a8001381bc83b8762d04be49bfe0df22b94884
rnd <ashokaditya.design@gmail.com>
2018-05-23 11:49:02 +0200

    related networks of a customer #10

--- .sql/customers/customerNetworks.sql
+++ .sql/customers/customerNetworks.sql
@@ -1,12 +1,15 @@
 select
-  customernetworkid,
-  uuid_customernetworkid "netuuid",
-  customerid,
-  uuid_customerid "couuid",
-  name,
-  kind,
-  net,
-  description
-from flow.customernetworks
-where customerid = $(customerid)
-order by customernetworkid, customerid asc
+  c.customernetworkid,
+  c.uuid_customernetworkid "netuuid",
+  a.customerid,
+  a.uuid_customerid "couuid",
+  c.name,
+  c.kind,
+  c.net,
+  c.description
+from flow.customers a
+join flow.customernetworks c
+on c.customernetworkid = any(a.networks)
+where a.customerid = $(customerid)
+and c.customernetworkid = ANY(a.networks::int[])
+order by c.customernetworkid, a.customerid asc
